a34b09e0bb45613132860356b0baf98fc54bf994
zhoutianliang <mfkgdyve@gmail.com>
2024-06-14 18:42:48 +0800

    modify fragment

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Controller\ToolController;
 use OctopusPress\Bundle\Bridge\Bridger;
 use OctopusPress\Bundle\Customize\AbstractControl;
+use OctopusPress\Bundle\Customize\ImageControl;
 use OctopusPress\Bundle\Entity\Post;
 use OctopusPress\Bundle\OctopusPressKernel;
 use OctopusPress\Bundle\Plugin\PluginInterface;
@@ -19,11 +20,19 @@ class Kernel extends OctopusPressKernel implements PluginInterface
         $page->addSupport('name')->addSupports();
 
         $devicons = json_decode(file_get_contents($this->getProjectDir() . '/devicons.json'), true);
-        $bridger->getMeta()->registerPost(Post::TYPE_POST, 'devicon', [], [
+        $meta = $bridger->getMeta();
+        $meta->registerPost(Post::TYPE_POST, 'devicon', [], [
             'type' => AbstractControl::SELECT_SEARCH,
             'label' => '主标签',
             'options' => $devicons,
         ]);
+        $meta->registerUser('donate_alipay', [], ImageControl::create('donate_alipay', '捐赠支付宝', [
+            "width" => 200,
+            "height"=> 0,
+        ]))->registerUser('donate_wechat', [], ImageControl::create('donate_wechat', '捐赠微信', [
+            "width" => 200,
+            "height"=> 0,
+        ]));
         $bridger->getHook()->add('_seo_sitemap_generator', function (array $urls, string $domain) {
             foreach (ToolController::$tools as $tool) {
                 $urls[] = $tool['href'];
